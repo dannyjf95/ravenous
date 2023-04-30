a98534615e80f6840eb6f7f9643708268b0fa70a
Daniel Frith <46321457+dannyjf95@users.noreply.github.com>
2023-04-30 19:05:57 +0100

    set up iteration for filtering searched options

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -2,21 +2,36 @@ import React from "react";
 import styles from './SearchBar.module.css' //modules file
 
 export function SearchBar() {
+
+ //create a JavaScript object to contain the key-value pairs for each sorting option
+ const sortOptions = {
+  "highest rating": "rating"
+ }
+
+ //iterate through options
+const sortOptionsList = () => {
+ return Object.keys(sortOptions).map((sortedOption) => (
+  <li key={sortOptions[sortedOption]}>{sortedOption}</li> //list item
+ ))
+}
+ 
+
+
+
   return (
-    <div className="sorting">
-      <div className="sorting-options">
-        <button>highest rated</button>
-        <button>open now</button>
+    <div className={styles.SearchBar}>
+      <div className={styles.filteredSearch}>
+        <ul>{sortOptionsList()}</ul> {/**it will have li inserted  */}
       </div>
 
-      <div className="search-bar">
-        <input id="searchTerm" placeholder="Search what you fancy" />
+      <div className={styles.searchBarInputs}>
+        <input placeholder={styles.Search} />
         {/** knife and fork favi */}
 
-        <input id="searchLocation" placeholder="Location" />
+        <input placeholder={styles.Location} />
         {/** location marker favi */}
 
-        <button id="search-button">search</button>
+        <button id={styles.searchButton}>search</button>
       </div>
     </div>
   );
