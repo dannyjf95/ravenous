ac40ddfcb26756d77fa9bc5853f02f19a917f081
Daniel Frith <46321457+dannyjf95@users.noreply.github.com>
2023-05-11 22:38:44 +0100

    make api calls using fetch get in Yelp.js module

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,30 @@
+import React, { useState } from "react";
 import styles from "./App.module.css";
 import { BusinessList } from "../BusinessList/BusinessList";
 import { SearchBar } from "../SearchBar/SearchBar";
-import { businessData } from "../BusinessList/businessData";
-import { yelpApi } from "../../utils/Yelp";
+
+import Yelp from "../../utils/Yelp";
+
 
 //passed down data through other file instead of hard coded in App
 function App() {
+  //remove hard code and pass real api business return state instead
+  const [yelpBusiness, setYelpBusiness] = useState([])
 
+   const results = async (searchTerm, location, sortBy) => {
+    const yelpBusinesses = await Yelp.searchYelp(searchTerm, location, sortBy)
+    //set the results to the state and pass to businessList instead of mock data
+    setYelpBusiness(yelpBusinesses)
+  }
   
+  // console.log(yelpBusiness)
+  
+
   return (
     <div className={styles.App}>
       <h1>ravenous</h1>
-      <SearchBar />
-      <BusinessList businessesData={businessData} />
+      <SearchBar   searchResults={results}/>
+      <BusinessList businessesData={yelpBusiness} />
     </div>
   );
 }
